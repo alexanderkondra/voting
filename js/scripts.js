9daf654a808d5e5eaf373b44e4cbfade75d67f71
alexander kondra and dustin herboldshimer <alexander@>
2019-03-28 13:33:49 -0700

    do some stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,56 @@
 $(document).ready(function() {
-    var age = parseInt(prompt("How old are you"));
-    console.log(age);
-    console.log(typeof age);
+    // var age = parseInt(prompt("How old are you"));
+    // console.log(age);
+    // console.log(typeof age);
+    //
+    // if (age >= 18) {
+    //   $('.over18').show();
+    //
+    //   }  else {
+    //     $('.under18').show();
+    //   }
+    // var height = parseInt(prompt("Enter your Height"));
+    // if (height <24) {
+    //   $('#shortest').show();
+    // }
+    // else if (height <48) {
+    //   $('#short').show();
+    // } else if (height <120){
+    //   $('#tall').show();
+    // }
+    // else {
+    //   alert("You're too tall to ride our stuff");
+    // }
 
-    if (age >= 18) {
-      $('.over18').show();
+    var height = parseInt(prompt("Enter your Height"));
 
-      }  else {
-        $('.under18').show();
-      }
+    if (height <24) {
+      $('#shortest').find("h2").addClass("starlight");
+    } else if (height < 48) {
+      $('#short').find("h2").addClass("starlight2");
+    } else {
+      $('#tall').find("h2").addClass("starlight");
+    }
+    console.log("have height");
+
+    $('body').find('.row').show();
+
+
+
+    // if (height <24) {
+    //   $('#shortest').show();
+    // }
+    // else if (height <48) {
+    //   $('#short').show();
+    // } else if (height <120){
+    //   $('#tall').show();
+    // }
+    // else {
+    //   alert("You're too tall to ride our stuff");
+    // }
+
+
+
+
 
     });
